c8694dab881358cbaf5530a96ad6376bf3f949b6
KarolPulawski <karol.j.pulawski@gmail.com>
2018-08-01 00:32:23 +0200

    Module 3 / Servlet 01 / Day 2 / Maven_a / Exercise 1,2,3,4,5.

--- src/main/java/pl/coderslab/Main.java
+++ src/main/java/pl/coderslab/Main.java
@@ -3,6 +3,13 @@ package pl.coderslab;
 
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
 
 public class Main {
 
@@ -11,5 +18,19 @@ public class Main {
     public static void main(String[] args) {
         logger.error("My error");
         logger.info("custom info");
+        getDataFromWebservice();
+    }
+
+    private static void getDataFromWebservice() {
+        Connection connect = Jsoup.connect("http://www.onet.pl/");
+        try {
+            Document document = connect.get();
+            Elements links = document.select("span.title");
+            for (Element elem : links) {
+                System.out.println(elem.text());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
